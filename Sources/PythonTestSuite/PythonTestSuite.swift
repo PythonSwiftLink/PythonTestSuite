7b3e0c08bcb7d93abe82491cf65b40c94d270da6
psychowasp <psychowaspx@gmail.com>
2023-09-15 12:43:11 +0200

    extra python libs added

--- Sources/PythonTestSuite/PythonTestSuite.swift
+++ Sources/PythonTestSuite/PythonTestSuite.swift
@@ -11,16 +11,25 @@ private var pythonIsRunning = false
 var pystdlib: URL {
     Bundle.module.url(forResource: "python_stdlib", withExtension: nil)!
 }
+var pyextra: URL {
+	Bundle.module.url(forResource: "python-extra", withExtension: nil)!
+}
 public func initPython() {
     if pythonIsRunning { return }
     pythonIsRunning.toggle()
 //    let resourcePath = "/Users/musicmaker/Library/Mobile Documents/com~apple~CloudDocs/Projects/xcode_projects/touchBay_files/touchBay/touchBay"
     let resourcePath: String
+	let python_extra: String
     if #available(macOS 13, *) {
         resourcePath = pystdlib.path()
     } else {
         resourcePath = pystdlib.path
     }
+	if #available(macOS 13, *) {
+		python_extra = pyextra.path()
+	} else {
+		python_extra = pyextra.path
+	}
     print(resourcePath)
     var config: PyConfig = .init()
     print("Configuring isolated Python for Testing...")
@@ -64,6 +73,11 @@ public func initPython() {
     status = PyWideStringList_Append(&config.module_search_paths, wtmp_str)
     
     PyMem_RawFree(wtmp_str)
+	
+	wtmp_str = Py_DecodeLocale(python_extra, nil)
+	status = PyWideStringList_Append(&config.module_search_paths, wtmp_str)
+	
+	PyMem_RawFree(wtmp_str)
     
     
     //PyImport_AppendInittab(makeCString(from: "fib"), PyInitFib)
